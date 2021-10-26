4e6841815281ba5f2db3da25cae551876da39093
Daniel Norton <nortond@flip1.engr.oregonstate.edu>
2021-10-26 12:45:47 -0700

    Add pages for each table and nav bar creation

--- app.js
+++ app.js
@@ -5,48 +5,61 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 PORT        = 8269;                 // Set a port number at the top so it's easy to change in the future
 var db = require('./database/db-connector')
+
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', exphbs({                     // Create an instance of the handlebars engine to process templates
+    extname: ".hbs"
+}));
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
 
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-        res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
-    });                                         // requesting the web site.
 
-// app.js 
+// app.js
+
+// app.js
 
 app.get('/', function(req, res)
     {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+app.get('/Facilities', function(req, res)
+    {
+        res.render('Facilities');               // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
+app.get('/FacilityTypes', function(req, res)
+    {
+        res.render('FacilityTypes');            // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
+app.get('/InspectionPersonnel', function(req, res)
+    {
+        res.render('InspectionPersonnel');      // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
+app.get('/Inspections', function(req, res)
+    {
+        res.render('Inspections');              // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
+app.get('/Inspectors', function(req, res)
+    {
+        res.render('Inspectors');               // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+app.get('/MaintenanceRecords', function(req, res)
+    {
+        res.render('MaintenanceRecords');       // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
 
 /*
